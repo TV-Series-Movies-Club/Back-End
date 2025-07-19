95c715975d70b1ddbce1a3a34319e47d336bd837
Billadams <nyamweno.billadams@gmail.com>
2025-07-20 00:55:21 +0300

    updated files

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
 
     posts = db.relationship('MoviePost', back_populates='user', cascade='all, delete')
     comments = db.relationship('Comment', back_populates='user', cascade='all, delete')
